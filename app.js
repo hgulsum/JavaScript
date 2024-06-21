26e4f89e5226060d7c0d3a09a0e95b0fbc236d87
hgulsum <gulsumhacili08@gmail.com>
2024-06-21 21:14:01 +0400

    we

--- app.js
+++ app.js
@@ -1,17 +1,1 @@
-//let a = "Hacili";
-//let b = "Gulsumxanim";
-//let fullName = a + " " + b;
-//console.log(fullName);
-
-console.log("HOMEWORK 1");
-
-let fullName = "Hacili Gulsumxanim";
-console.log(fullName);
-console.log(fullName.split(" ")[0][0]);
-console.log(fullName.split(" ")[1][0]);
-
-console.log("HOMEWORK 2");
-let fulllName = "Gulsumxanim Hacili";
-let namefull = "Hacili Gulsumxanim";
-console.log(fulllName);
-console.log(fulllName.replace(fulllName, namefull));
+console.log("aghffyf");
